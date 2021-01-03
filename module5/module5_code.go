92531780bdfa76eefab8ceda1f43a7cdbaacc973
SebastianMOtero <34726803+SebastianMOtero@users.noreply.github.com>
2021-01-03 02:05:01 -0300

    fifth issue

--- module5/module5_code.go
+++ module5/module5_code.go
@@ -5,6 +5,7 @@ import (
 )
 
 // GetExampleDotCom uses the "net/http" package to send a GET request to example.com
+/*
 func GetExampleDotCom() {
 	resp, err := http.Get("http://google.com/")
 	if err != nil {
@@ -13,3 +14,4 @@ func GetExampleDotCom() {
 
 	defer resp.Body.Close()
 }
+*/
